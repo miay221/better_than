16b53d6bf262fd1453573b761c0d3c5257e9831b
Miniland289 <wjdwjd1945@gmail.com>
2024-11-23 21:39:40 +0900

    .

--- app/chatbot/views.py
+++ app/chatbot/views.py
@@ -24,9 +24,14 @@ def analyze_message(request):
             max_val= max(values[0])
             print('가장 높은 확률', max_val)
 
+            range_values = list(range(result['predicted_class']))
+            range_values.append(result['predicted_class'] + 1)  # 마지막 값에 1 추가
+            print('예측된 클래스:', range_values)
+
             # 결과를 세션에 저장 (동기 방식)
             request.session['analysis_result'] = result
             request.session['max_val'] = max_val
+            request.session['range'] = range_values
 
             # 분석 결과, 응답 반환
             return JsonResponse({
@@ -41,12 +46,14 @@ def analyze_message(request):
 
 def index(request):
     # 세션에서 메시지와 분석 결과 가져오기
-    message = request.session.pop('user_message', None)
-    result = request.session.pop('analysis_result', None)
-    max_val = request.session.pop('max_val', None)
+    message = request.session.get('user_message', None)
+    result = request.session.get('analysis_result', None)
+    max_val = request.session.get('max_val', None)
+    range_values = request.session.get('range', None)
 
     return render(request, 'chatbot/index.html', {
         'message': message,
         'result': result,
-        'max_val': max_val
+        'max_val': max_val,
+        'range':range_values,
     })
